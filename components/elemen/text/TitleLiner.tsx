d3d865a80651b5d1ec46cca54e20242a11c91bf2
madamroger007 <madamroger007@github.com>
2024-06-17 21:50:55 +0700

    fit: Menambahkan text components

--- components/elemen/text/TitleLiner.tsx
+++ components/elemen/text/TitleLiner.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+  import React from 'react'
 
-const TitleLiner = ({title}:{title:string}) => {
-  return (
-    <div className='max-w-[200px] flex flex-col gap-3'>
+  const TitleLiner = ({ title }: { title: string }) => {
+    return (
+      <div className='max-w-[200px] flex flex-col gap-2 text-center'>
         <h1 className='font-raleway font-extrabold text-3xl dark:text-gray-300 text-gray-700'>{title}</h1>
-        <div className='bg-[#7456FF] w-full h-1.5 rounded-md'></div>
-    </div>
-  )
-}
+        <div className='bg-[#3a2694] w-full h-[5.5px] rounded-md'></div>
+      </div>
+    )
+  }
 
-export default TitleLiner
+  export default TitleLiner
