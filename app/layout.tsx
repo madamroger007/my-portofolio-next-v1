b5576fc35ba3cf9e9b37bff6ed771585ce90fb4a
madamroger007 <adamsetiadijr07@gmail.com>
2024-08-19 09:40:39 +0700

    fix: Menambahakan sitemap, robots dan memperbaiki bebrapa kode di tag meta layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,25 @@ import { ThemeProvider } from '@/context/ThemeContext';
 import { env } from 'process';
 
 export const metadata: Metadata = {
+  metadataBase: new URL(`${process.env.URL_PRODUCTION}`),
+  generator: "Next.js",
+  applicationName: 'Website Portofolio',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Adam', 'Adam setiadi', 'Madam', 'MadamRoger', "Madamroger", "madamroger"],
+  authors: [{ name: 'Adam', url: process.env.URL_PRODUCTION }],
+  creator: 'Adam Setiadi',
+  publisher: 'Adam Setiadi',
+  formatDetection: {
+    email: true,
+    address: true,
+  },
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+      'id-ID': '/id-ID'
+    },
+  },
   title: {
     absolute: "",
     default: "Madamroger",
@@ -13,7 +32,7 @@ export const metadata: Metadata = {
   description: "Website portofolio Madamroger",
   openGraph: {
     title: "Madamroger",
-    description: "Website portofolio Madamroger",
+    description: "Hai! Nama saya Adam Setiadi, saat ini saya tinggal di Bandung, Jawa Barat, Indonesia. Saya sedang menempuh pendidikan di Universitas Bale Bandung",
     url: env.URL_PRODUCTION, // Ganti dengan URL situs Anda
     siteName: "Madamroger",
     locale: "en_US",
@@ -22,7 +41,7 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Madamroger",
-    description: "Website portofolio Madamroger"
+    description: "WHai! Nama saya Adam Setiadi, saat ini saya tinggal di Bandung, Jawa Barat, Indonesia. Saya sedang menempuh pendidikan di Universitas Bale Bandung"
   }
 };
 
---
+++ app/sitemap.ts
@@ -0,0 +1,29 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${process.env.URL_PRODUCTION}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${process.env.URL_PRODUCTION}/skill`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${process.env.URL_PRODUCTION}/project`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },{
+        url: `${process.env.URL_PRODUCTION}/contact`,
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+  ]
+}
