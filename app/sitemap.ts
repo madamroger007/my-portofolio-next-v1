2bd5e2ac73df67a2fe9cbb4aa3dbe468184cbab2
madamroger007 <adamsetiadijr07@gmail.com>
2024-10-03 21:01:39 +0700

    fix:sitemap code and refaktor versi 2

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,32 +7,27 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: `${baseUrl}`,
       lastModified: new Date().toISOString(), // Ensures proper date format
-      changeFrequency: 'yearly',
-      priority: 1,
+   
     },
     {
       url: `${baseUrl}/about`,
       lastModified: new Date().toISOString(),
-      changeFrequency: 'yearly',
-      priority: 0.9,
+     
     },
     {
       url: `${baseUrl}/skill`,
       lastModified: new Date().toISOString(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
+   
     },
     {
       url: `${baseUrl}/project`,
       lastModified: new Date().toISOString(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
+
     },
     {
       url: `${baseUrl}/contact`,
       lastModified: new Date().toISOString(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
+     
     },
   ];
 }
